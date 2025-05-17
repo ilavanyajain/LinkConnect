bb8b469633089cd3f2dbff4dbd2a95c31462e642
ilavanyajain <lavanyajain2434@gmail.com>
2025-05-18 02:12:09 +0530

    new changes

--- linkedin_bot.py
+++ linkedin_bot.py
@@ -93,8 +93,8 @@ class LinkedInBot:
 
         with sync_playwright() as p:
             browser = p.chromium.launch(
-                headless=False,
-                args=['--disable-blink-features=AutomationControlled']
+                headless=True,
+                args=['--disable-blink-features=AutomationControlled', '--no-sandbox']
             )
             context = browser.new_context(
                 viewport={'width': 1920, 'height': 1080},
